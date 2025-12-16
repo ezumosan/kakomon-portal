4b1c5d4c5d36acbb2e4e6146c7f37c15ee6f6ea6
Rikuto Saito <ezumo@Ezumos-MacBook-Air.local>
2025-12-16 14:32:59 +0900

    first website

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>画像とCSSのテスト</title>
+
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+
+    <main>
+        <h1>Webサイトへようこそ</h1>
+        <p>以下に画像が表示されます。</p>
+
+        <img src="/images/logo.jpg" alt="サイトのロゴ" width="300">
+    </main>
+
+</body>
+</html>
